ece88f5fb2ca549ee560a10bfe08bb7849aee557
Shaira Mallari <mallarishaira@gmail.com>
2021-05-08 16:44:13 +0800

    First commit

---
+++ mevnproject/src/components/IndexComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">Index Component</div>
+
+                <div class="card-body">
+                    I'm an index component.
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
